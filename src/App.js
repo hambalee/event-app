03189dd7c301037dab427abc15e730f9579c7984
hambalee <hambalee@outlook.com>
2020-04-01 22:33:33 +0700

    install router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Home from "./containers/Home";
+import Login from "./containers/Login";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import About from "./containers/About";
+import NavBar from "./components/NavBar";
+import NotFound from "./containers/error/NotFound";
+import Admin from "./containers/Admin";
+import Event from "./containers/Event";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <NavBar />
+
+          <Switch>
+            <Route exact path="/home">
+              <Home />
+            </Route>
+            <Route exact path="/about">
+              <About />
+            </Route>
+            <Route exact path="/login">
+              <Login />
+            </Route>
+            <Route exact path="/admin">
+              <Admin />
+            </Route>
+            <Route exact path="/event">
+              <Event />
+            </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route>
+              <NotFound />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+export default class NavBar extends Component {
+  render() {
+    return (
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
